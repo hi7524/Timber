a60e702c1d616fe2793d140d475d2caadc817d24
김혜인 <rjqnrdl7524@naver.com>
2025-06-27 09:08:15 +0900

    managers
    싱글톤 패턴 이용한 매니저 구현

---
+++ main.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include "InputMgr.h"
+#include "ResourceMgr.h"
+#include "SFML/Graphics.hpp"
+#include "SFML/Audio.hpp"
+
+int main()
+{
+   
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    ResourceMgr<sf::Texture>::Instance().Load(
+        {
+            "graphics/player.png" ,
+            "graphics/axe.png" ,
+            "graphics/bee.png" ,
+            "graphics/branch.png" ,
+            "graphics/cloud.png" ,
+            "graphics/log.png" ,
+            "graphics/rip.png" ,
+            "graphics/tree.png"
+        }
+            );
+    sf::Sprite sprite;
+    sprite.setTexture(ResourceMgr<sf::Texture>::Instance().Get("graphics/player.png"));
+    
+    while (window.isOpen()) 
+    {
+        sf::Event event;
+
+        InputMgr::Clear();
+
+        while (window.pollEvent(event)) 
+        {
+            if (Sevent.type == sf::Event::Closed) 
+            {
+                window.close();
+            }
+
+            InputMgr::UpdateEvent(event);
+        }
+
+        //Update
+        InputMgr::Update(0);
+
+        if (InputMgr::GetKeyDown(sf::Keyboard::A)) 
+        {
+            std::cout << "Key Down: A" << std::endl;
+        }
+        if (InputMgr::GetKey(sf::Keyboard::B)) 
+        {
+            std::cout << "Key Held: B" << std::endl;
+        }
+        if (InputMgr::GetKeyUp(sf::Keyboard::A)) 
+        {
+            std::cout << "Key Up: A" << std::endl;
+        }
+
+        //Draw
+        window.clear();
+
+        
+        window.draw(sprite);
+        
+        
+        window.display();
+        
+    }
+
+    return 0;
+}
