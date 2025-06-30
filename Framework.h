14e43319a3d816c648b04a59ffb7fb7a1dd81209
김혜인 <rjqnrdl7524@naver.com>
2025-06-30 11:37:13 +0900

    Framework 구현

--- Framework.h
+++ Framework.h
@@ -15,13 +15,28 @@ protected:
 	sf::Clock clock;
 	float timeScale = 1.0f;
 
-	float time;
-	float deltaTime;
+	float time = 0.0f;
+	float deltaTime = 0.0f;
 
-	float realTime;
-	float realDeltaTime;
+	float realTime = 0.0f;
+	float realDeltaTime = 0.0f;
+
+	std::vector<std::string> texIds;
+	std::vector<std::string> fontIds;
+	std::vector<std::string> soundIds;
 
 public:
+	sf::RenderWindow& GetWindow() { return window; }
+
+	sf::Vector2u GetWindowSize() const { return window.getSize(); }
+	sf::Vector2f GetWindowSizeF() const { return (sf::Vector2f)window.getSize(); }
+	sf::FloatRect GetWindowBounds() const
+	{
+		sf::Vector2f size = GetWindowSizeF();
+		return sf::FloatRect(0.0f, 0.0f, size.x, size.y);
+	}
+
+
 	float GetTimeScale() const { return timeScale; }
 	void SetTimeScale(float scale) { timeScale = scale; }
 
@@ -30,7 +45,7 @@ public:
 	float GetRealTime() const { return realTime; }
 	float GetRealDeltaTime() const { return realDeltaTime; }
 
-	virtual void Init();
+	virtual void Init(int w, int h, const std::string& t);
 	virtual void Do(); // main ∑Á«¡
 	virtual void Release();
 };
--- main.cpp
+++ main.cpp
@@ -1,53 +1,11 @@
 #include "stdafx.h"
-
+#include "Framework.h"
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(1920, 1080), "TIMBER");
-
-    TEXTURE_MGR.Load({
-        "graphics/player.png",
-        "graphics/axe.png",
-        "graphics/bee.png",
-        "graphics/branch.png",
-        "graphics/cloud.png",
-        "graphics/log.png",
-        "graphics/rip.png",
-        "graphics/tree.png"
-    });
-
-    FONT_MGR.Load(
-        "fonts/KOMIKAP_.ttf"
-    );
-
-    SCENE_MGR.Init();
-
-    while (window.isOpen()) 
-    {
-        sf::Event event;
-
-        InputMgr::Clear();
-
-        while (window.pollEvent(event)) 
-        {
-            if (event.type == sf::Event::Closed) 
-            {
-                window.close();
-            }
-
-            InputMgr::UpdateEvent(event);
-        }
-
-        //Update
-        InputMgr::Update(0.0f);
-        SCENE_MGR.Update(0.0f);
-
-        //Draw
-        window.clear();
-        SCENE_MGR.Draw(window);
-        
-        window.display();
-    }
+    FRAMEWORK.Init(1920, 1080, "TIMBER_FRAMEWORK");
+    FRAMEWORK.Do();
+    FRAMEWORK.Release();
 
     return 0;
 }
